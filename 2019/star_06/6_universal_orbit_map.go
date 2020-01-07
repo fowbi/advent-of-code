368c30a5f2d50b79b153c6bcbf146558d362722f
Tobi Beernaert <tobi@magier.be>
2020-01-07 12:54:10 +0900

    :star: Day 06 - part 02
    https://adventofcode.com/2019/day/6
    
    Signed-off-by: Tobi Beernaert <tobi@magier.be>

--- 2019/star_06/6_universal_orbit_map.go
+++ 2019/star_06/6_universal_orbit_map.go
@@ -50,21 +50,60 @@ func determineOrbits(spaceObject SpaceObject, spaceObjects []SpaceObject, route 
 		return route
 	}
 
-	route = append(route, nextSpaceObject)
+	route = prependSpaceObject(route, nextSpaceObject)
 
 	return determineOrbits(nextSpaceObject, spaceObjects, route)
 }
 
+func Min(x, y int) int {
+	if x > y {
+		return y
+	}
+	return x
+}
+
+func prependSpaceObject(x []SpaceObject, y SpaceObject) []SpaceObject {
+	x = append(x, SpaceObject{})
+	copy(x[1:], x)
+	x[0] = y
+
+	return x
+}
+
 func main() {
-	var input = readInput("input-test-2.txt")
+	var input = readInput("input.txt")
 	var spaceObjects = extractSpaceObjects(input)
 	orbits := 0
+
+	var me, santa []SpaceObject
 
 	for _, spaceObject := range spaceObjects {
 		var r = determineOrbits(spaceObject, spaceObjects, []SpaceObject{spaceObject})
 		orbits += len(r)
+
+		if spaceObject.orbittingObject == "YOU" {
+			me = r
+		}
+
+		if spaceObject.orbittingObject == "SAN" {
+			santa = r
+		}
 	}
 
+	var l = Min(len(me), len(santa))
+	var pointOfDifference = 0
+
+	for i := 0; i < l; i++ {
+		if me[i] != santa[i] {
+			pointOfDifference = i
+			break
+		}
+	}
+
+	var orbitalTransfers = (len(me) - 1) + (len(santa) - 1) - (pointOfDifference * 2)
+
 	fmt.Println("solution part 1 :", orbits)
+
+	fmt.Println("solution part 2 :", orbitalTransfers)
 	os.Exit(0)
 }
